8bd5fdca57bf67d715db25aec4fc255a719c5f3e
Anton Uzhva <anton.itec91@gmail.com>
2025-01-14 22:36:50 +0200

    initial commit

---
+++ src/main/java/org/anton_u/tripium_test_task/TripiumTestTaskApplication.java
@@ -0,0 +1,18 @@
+package org.anton_u.tripium_test_task;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripiumTestTaskApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripiumTestTaskApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // TODO Complete the logic
+    }
+}
---
+++ src/test/java/org/anton_u/tripium_test_task/TripiumTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anton_u.tripium_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripiumTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
